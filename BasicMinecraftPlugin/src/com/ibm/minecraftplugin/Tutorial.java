e952f4019a22573c9658b6b7f70a8aaf93b12e34
Kyle Brown <kgb1001001@aol.com>
2015-03-09 20:46:37 -0400

    [initial version]

---
+++ BasicMinecraftPlugin/src/com/ibm/minecraftplugin/Tutorial.java
@@ -0,0 +1,20 @@
+package com.ibm.minecraftplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Tutorial extends JavaPlugin {
+	
+    public void onEnable(){
+        getLogger().info("Hello World!");
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+    	if (cmd.getName().equalsIgnoreCase("hello")) {
+    		sender.sendMessage("world");
+    	}
+    	return false;
+    }
+}
